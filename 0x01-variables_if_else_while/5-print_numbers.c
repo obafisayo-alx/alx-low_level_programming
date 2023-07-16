0af99762ff30944b84bfd93e757f129c9598c1ac
obafisayo-alx <sekaniisjoy@gmail.com>
2023-07-16 16:15:44 -0700

    Added  5--print_numbers.c file

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Always returns 0
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a <= 9; a++)
+		printf("%d\n", a);
+	return (0);
+}

a0ca5727fdcce20531922ef99e0b364b76daae4f
obafisayo-alx <sekaniisjoy@gmail.com>
2023-08-28 15:09:46 -0700

    test 2

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,11 @@
 #ifndef _DOG_H_
 #define _DOG_H_
+/**
+ * struct dog - structure containing characteristics of a dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: name of owner
+ */
 
 struct dog
 {

e3c33d5ad141f6f6c1896920271f15eaeb84bcb1
obafisayo-alx <sekaniisjoy@gmail.com>
2023-09-05 14:25:00 -0700

    corrected betty-sttyle

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -9,30 +9,26 @@
  * file `filename'.
  * @filename: name of the file to append to
  * @text_content: content to append to `filename'
- *
+ * @append_tect_to_file: appends text from 'text_content'to file end
  * Return: 1 on success, -1 if `write' or `open' fails
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd,  i;
+	int fd, i;
 
 	if (filename == NULL)
 		return (-1);
 
-    fd = open(filename, O_APPEND | O_WRONLY);
-    if (fd == -1)  
-        return (-1);
-
-    for ( i = 0; text_content && text_content[i]; i++)
-        ;
-    
-    if (write(fd, text_content, i) != i)
-    {
-        close(fd);
-        return (-1);
-    }
-
-    close(fd);
-    return (1);
-
-} 
+	fd = open(filename, O_APPEND | O_WRONLY);
+	if (fd == -1)
+		return (-1);
+	for (i = 0; text_content && text_content[i]; i++)
+		;
+	if (write(fd, text_content, i) != i)
+	{
+		close(fd);
+		return (-1);
+	}
+	close(fd);
+	return (1);
+}

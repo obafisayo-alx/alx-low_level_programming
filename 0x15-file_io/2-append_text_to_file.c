4735f1c787b4567729d56211351ea8afbca4caee
obafisayo-alx <sekaniisjoy@gmail.com>
2023-09-05 21:20:24 +0100

    midified the 3rd file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -4,37 +4,36 @@
 #include <sys/types.h>
 #include <stdlib.h>
 
+ /**
+ * append_text_to_file - append text `text_content' to the end of
+ * file `filename'.
+ * @filename: name of the file to append to
+ * @text_content: content to append to `filename'
+ *
+ * Return: 1 on success, -1 if `write' or `open' fails
+ */
 int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd;
 	size_t i;
 	ssize_t bytes_to_write;
-    char *buffer;
 
 	if (filename == NULL)
 		return (-1);
 
-    if (buffer == NULL)
+    fd = open(fd, O_APPEND | O_WRONLY);
+    if (fd == -1)  
         return (-1);
 
     for ( i = 0; text_content && text_content[i]; i++)
         ;
     
-    buffer = malloc(sizeof(char) * i);
-    fd = open(fd, O_APPEND | O_WRONLY);
-    if (fd == -1)
-    {   
-        free(buffer);
-        return (-1);
-    }
-    
     if (text_content == NULL)
     {
-        free(buffer);
         close(fd);
-        write(fd, text_content, i);
+        return (-1);
     }  else 
-        bytes_to_write = write(fd, buffer, sizeof(buffer) - 1);
+        bytes_to_write = write(fd, text_content, i);
 
     close(fd);
     if (bytes_to_write == -1)

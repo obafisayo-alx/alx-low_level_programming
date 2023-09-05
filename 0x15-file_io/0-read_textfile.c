af0bf883995fb747cf0e0b46ff524d377d1659c6
obafisayo-alx <sekaniisjoy@gmail.com>
2023-09-05 13:43:53 +0100

    added a README file and created task 0 file

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,59 @@
+#include "main.h"
+#include <sys/stat.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * read_textfile - read a text file and print to STDOUT.
+ * @filename: path of filename to read.
+ * @letters: number of letters to read and write.
+ *
+ * Return: 0 if `filename' is NULL orcannot be opened or read,
+ * 0 if call to write fails or is less than the expected number
+ * of bytes, or the total number of bytes used to call write.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+    if (filename = NULL)
+    {
+            return (0);
+    }
+
+    char *buffer = malloc(sizeof(char) * letters);
+    if (buffer == NULL)
+    {
+            return (0);
+    }
+
+    int fd = open("filename", O_RDONLY);
+    if ( fd == -1 )
+    {
+            free(buffer);
+            return (0);
+    }
+
+    ssize_t bytes_read = read(fd, buffer, letters);
+    if (bytes_read == -1)
+    {
+            free(buffer);
+            return (0);
+    }
+
+    ssize_t bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+    if (bytes_written == -1)
+    {
+            free(buffer);
+            return (0);
+    }
+    
+    if (bytes_written != bytes_read)
+    {
+            return (0);
+    }
+
+    return (bytes_read);
+    
+    close(fd);
+}

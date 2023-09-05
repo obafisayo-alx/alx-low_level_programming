da41a5229fe184877b761a4ea099eb09d6f76081
obafisayo-alx <sekaniisjoy@gmail.com>
2023-09-06 00:58:18 +0100

    mss

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,11 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
-
-
 #include <sys/types.h>
-
 #define BUFFER_SIZE 1024
-int create_file(const char *filename, char *text_content);
 ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-#endif /** MAIN_H*/
+#endif

b5298c9255ea49db4c332b95a55fd281f986628b
obafisayo-alx <sekaniisjoy@gmail.com>
2023-09-05 11:00:02 -0700

    lets see

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,22 +13,22 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-       	int fd;
-    	size_t length_of_text;
-    	ssize_t bytes_to_write;
+	int fd;
+	size_t length_of_text;
+	ssize_t bytes_to_write;
 
-    	if (filename == NULL)
-        	return (-1);
-    	for ( length_of_text = 0; text_content && text_content[length_of_text] ; length_of_text++)
-        ;
-    	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-    	if (fd == -1)
-        	return (-1);
-    	if (text_content == NULL)
-        	text_content = ""; 
-    	bytes_to_write = write(fd, text_content, length_of_text);
-    	close(fd);
-    	if (bytes_to_write == -1)
-        	return (-1);
-    	return (1);
+	if (filename == NULL)
+		return(-1);
+	for (length_of_file = 0; text_content && text_content[length_of_text]; length_of_text++)
+		;
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+	if (text_content == NULL)
+		text_content = "";
+	bytes_to_write = write(fd, text_content, length_of_text);
+	close(fd);
+	if (bytes_to_write == -1)
+		return (-1);
+	return (1);
 }

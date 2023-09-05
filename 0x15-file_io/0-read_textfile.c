15b6f09d723b18b8d2ce8b0165f9f3b6c9dc1d50
obafisayo-alx <sekaniisjoy@gmail.com>
2023-09-05 05:57:14 -0700

    edited from sandbox

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,9 +1,8 @@
-#include "main.h"
+#include <sys/types.h>
 #include <sys/stat.h>
-#include <unistd.h>
 #include <fcntl.h>
 #include <stdlib.h>
-#include <stdio.h>
+#include <unistd.h>
 
 /**
  * read_textfile - read a text file and print to STDOUT.
@@ -16,44 +15,36 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-        if (filename = NULL)
-        {
-                return (0);
-        }
+	char *buf;
+	int fd;
+	ssize_t r, w;
 
-        char *buffer = malloc(sizeof(char) * letters);
-        if (buffer == NULL)
-        {
-                return (0);
-        }
-
-        int fd = open("filename", O_RDONLY);
-        if ( fd == -1 )
-        {
-                free(buffer);
-                return (0);
-        }
-
-        ssize_t bytes_read = read(fd, buffer, letters);
-        if (bytes_read == -1)
-        {
-                free(buffer);
-                return (0);
-        }
-
-        close(fd);
-        
-        ssize_t bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
-        if (bytes_written == -1)
-        {
-                free(buffer);
-                return (0);
-        }
-        
-        if (bytes_written != bytes_read)
-        {
-                return (0);
-        }
-
-        return (bytes_read);
+	if (filename == NULL)
+		return (0);
+	buf = malloc(sizeof(char) * letters);
+	if (buf == NULL)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	{
+		free(buf);
+		return (0);
+	}
+	r = read(fd, buf, letters);
+	if (r == -1)
+	{
+		free(buf);
+		close(fd);
+		return (0);
+	}
+	close(fd);
+	w = write(STDOUT_FILENO, buf, r);
+	if (w == -1)
+	{
+		free(buf);
+		return (0);
+	}
+	if (w != r)
+		return (0);
+	return (r);
 }

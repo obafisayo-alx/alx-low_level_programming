b769b35d2147bb3cd0974460b6627eeb3bb28ac9
obafisayo-alx <sekaniisjoy@gmail.com>
2023-09-06 15:13:22 +0100

    ok

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -9,7 +9,19 @@
  * @file: Name of the file that can't be read
  */
 void openfiles(char *file_from, char *file_to, int *fd_from, int *fd_to);
-void writefiles(int *fd_from, int *fd_to);
+void writefiles(int *fd_from, int *fd_to, char *file_from, char *file_to);
+void file_from_fail(char *file)
+{
+    dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file);
+	exit(98);
+}
+
+void file_to_fail(char *file)
+{
+    dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file);
+	exit(99);
+}
+
 int main(int ac, char **av)
 {
 	if (ac != 3)
@@ -21,8 +33,12 @@ int main(int ac, char **av)
 	char *file_to = av[2];
 	int fd_from, fd_to;
 
+    if (file_from == NULL)
+        file_from_fail(file_from);
+    if (file_to == NULL)
+        file_to_fail(file_to);
 	openfiles(file_from, file_to, &fd_from, &fd_to);
-	writefiles(fd_from, fd_to);
+	writefiles(fd_from, fd_to, file_from, file_to);
 	if (close(fd_from) == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %s\n", fd_from);
@@ -38,45 +54,33 @@ int main(int ac, char **av)
 
 void openfiles(char *file_from, char *file_to, int *fd_from, int *fd_to)
 {
-	mode_t old_umask = umask(0);
-
 	*fd_from = open(file_from, O_RDONLY);
 	if (*fd_from == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
-		exit(98);
+		file_from_fail(*fd_from);
 	}
 	*fd_to = open(file_to, O_TRUNC | O_CREAT | O_WRONLY, 0664);
-	umask(old_umask);
 	if (*fd_to == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
-		close(fd_from);
-		exit(99);
+		file_to_fail(*fd_to);
 	}
 }
 
-void writefiles(int *fd_from, int *fd_to)
+void writefiles(int *fd_from, int *fd_to, char *file_from, char *file_to)
 {
 	ssize_t bytes_r, bytes_w;
 	char *buffer[BUFFER_SIZE];
 
-	while ((bytes_r = read(fd_from, buffer, sizeof(buffer)) > 0))
+    bytes_r = read(fd_from, buffer, BUFFER_SIZE);
+    if (bytes_r == -1)
+        file_from_fail(file_from);
+	while ((bytes_r > 0))
 	{
 		bytes_w = write(fd_to, buffer, bytes_r);
-		if (bytes_w == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't write to fd %d\n", fd_to);
-			close(fd_from);
-			close(fd_to);
-			exit(99);
-		}
-	}
-	if (bytes_r == -1)
-	{
-		close(fd_from);
-		dprintf(STDERR_FILENO, "Error: Can't read from fd %d\n", fd_from);
-		close(fd_to);
-		exit(98);
+		if (bytes_w != bytes_r)
+			file_to_fail(file_to);
+        bytes_r = read(fd_from, buffer, BUFFER_SIZE);
+        if (bytes_r == -1)
+            file_from_fail(file_from);
 	}
 }

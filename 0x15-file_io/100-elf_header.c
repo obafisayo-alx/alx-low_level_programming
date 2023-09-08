40c90832692e723afe9a3445b571cc6327e1a09f
obafisayo-alx <sekaniisjoy@gmail.com>
2023-09-09 00:57:34 +0100

    mssg

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,10 +1,8 @@
 #include <unistd.h>
 #include <fcntl.h>
-#include <sys/stat.h>
-#include <sys/types.h>
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
 #include <elf.h>
 
 #define ELF_MAGIC "\x7F\x45\x4C\x46"
@@ -13,41 +11,42 @@ int elf_file(const char *filename);
 void read_elf_file(const char *filename);
 
 /**
- * print_err - This function is used to print and error message
+ * print_err - This function is used to print an error message
  * with an exit code of 98
  * @filename: This is the name of the file that caused an error
-*/
-void print_err(filename)
+ */
+void print_err(const char *filename)
 {
-    printf("Error: This is not an ELF file %s\n", filename);
+    fprintf(stderr, "Error: This is not an ELF file %s\n", filename);
     exit(98);
 }
 
 /**
  * main - This is the main function
- * @ac: this is the number of arguments counted
- * @av: This is an array of argument vectors passed
+ * @argc: this is the number of arguments counted
+ * @argv: This is an array of argument vectors passed
  * return is always 0
-*/
-int main(int ac, char **av)
+ */
+int main(int argc, char **argv)
 {
-    if (ac != 2)
-        return (-1);
-    if (elf_file(av[1]))
+    if (argc != 2)
+        return (1);
+    if (elf_file(argv[1]))
     {
         printf("ELF Header:\n\t");
-        read_elf_file(av[1]);
-    } else
-        print_err(av[1]);
+        read_elf_file(argv[1]);
+    }
+    else
+        print_err(argv[1]);
     return (0);
 }
 
 /**
- * elf_file - is a funtion that checks if the passed in file name is an 
+ * elf_file - is a function that checks if the passed-in file name is an
  * ELF file
  * @filename: This is the filename passed as an argument into the file
-*/
-int elf_file (const char *filename)
+ */
+int elf_file(const char *filename)
 {
     int fd = open(filename, O_RDONLY);
     if (fd == -1)
@@ -64,15 +63,15 @@ int elf_file (const char *filename)
     if (lseek(fd, 0, SEEK_SET) == -1)
     {
         close(fd);
-        print_error(filename);
+        print_err(filename);
     }
     ssize_t byt_r = read(fd, magic_bytes, sizeof(magic_bytes));
-    if (byt_r != sizeof(magic_bytes)) 
+    if (byt_r != sizeof(magic_bytes))
     {
         close(fd);
         print_err(filename);
     }
-    if (memcmp(magic_bytes, ELF_MAGIC, sizeof(magic_bytes)) == 0) 
+    if (memcmp(magic_bytes, ELF_MAGIC, sizeof(magic_bytes)) == 0)
     {
         close(fd);
         return (1);
@@ -91,33 +90,33 @@ void read_elf_file(const char *filename)
     int fd = open(filename, O_RDONLY);
     if (fd == -1)
         print_err(filename);
-    
+
     Elf64_Ehdr elf_header;
 
     ssize_t byt_r = read(fd, &elf_header, sizeof(elf_header));
-    if (byt_r != sizeof(elf_header)) 
+    if (byt_r != sizeof(elf_header))
     {
         close(fd);
         print_err(filename);
     }
     if (elf_header.e_ident[EI_CLASS] != ELFCLASS64)
     {
-        printf("Not a 64-bit ELF file\n")
+        printf("Not a 64-bit ELF file\n");
         close(fd);
         return;
     }
     printf("Magic:");
     int i;
 
-    for ( i = 0; i < EI_NIDENT; i++)
+    for (i = 0; i < EI_NIDENT; i++)
     {
         printf("%02X ", elf_header.e_ident[i]);
     }
-    printf("\n")
+    printf("\n");
     printf("Class: %d-bit\n", elf_header.e_ident[EI_CLASS] == ELFCLASS64 ? 64 : 32);
     printf("Data: %s\n", elf_header.e_ident[EI_DATA] == ELFDATA2LSB ? "Little-endian" : "Big-endian");
     printf("Version: %d\n", elf_header.e_ident[EI_VERSION]);
-    printf("OS/ABI: %d\n", elf_header.e_ident[EL_OSABI]);
+    printf("OS/ABI: %d\n", elf_header.e_ident[EI_OSABI]);
     printf("ABI Version: %d\n", elf_header.e_ident[EI_ABIVERSION]);
     printf("Type: %d\n", elf_header.e_type);
     printf("Entry point address: 0x%lx\n", (unsigned long)elf_header.e_entry);

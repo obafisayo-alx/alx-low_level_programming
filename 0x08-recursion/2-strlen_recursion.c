1262b5a8e36deb1219e4a0b283972400ea1cf853
obafisayo-alx <sekaniisjoy@gmail.com>
2023-09-29 02:47:41 +0100

    added the second file

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,6 +1,16 @@
 #include "main.h"
 
+/**
+ * _strlen_recursion - This is the recursive function.
+ * @s: This is the string to be passed.
+*/
 int _strlen_recursion(char *s)
 {
-    
+    int len = 0;
+    if (*s != '\0')
+    {
+        len ++;
+        len += _strlen_recursion(s + 1);
+    }
+    return (len);
 }

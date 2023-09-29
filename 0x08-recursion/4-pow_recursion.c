ec25efee33c4bd3c37e3375de6783e7a550179d5
obafisayo-alx <sekaniisjoy@gmail.com>
2023-09-29 03:41:01 +0100

    added the second file

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - This is the recursion function.
+ * @x: This is the value.
+ * @y: This is the power.
+*/
+int _pow_recursion(int x, int y)
+{
+    if (y < 0)
+        return (-1);
+    if (y == 0)
+    {
+        return (1);
+    } else
+    {
+        return (x * _pow_recursion(x, (y - 1)));
+    }
+}

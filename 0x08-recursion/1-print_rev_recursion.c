9cf6d68c433e0020f9e5e6f3b95385aa05e925c6
obafisayo-alx <sekaniisjoy@gmail.com>
2023-09-29 02:24:40 +0100

    added the second file

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - This is the function for this file.
+ * @s: This is the string to be passed.
+*/
+void _print_rev_recursion(char *s)
+{
+    if (*s != '\0')
+    {
+        _print_rev_recursion(s + 1);
+        print_char(*s);
+    }
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -3,5 +3,7 @@
 #include <unistd.h>
 
 void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+void print_char(char c);
 
 #endif /**_MAIN_H_*/

2cfbb5aa7e738a8b7e11666689a809f14f218349
obafisayo-alx <sekaniisjoy@gmail.com>
2023-09-29 01:20:22 +0100

    ok

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+void print_char(char c)
+{
+    write(1, &c, 1);
+}
+void _puts_recursion(char *s)
+{
+    int i = 0;
+
+    if (*s != '\0')
+    {
+        print_char(*s);
+        _puts_recursion(s + 1);
+    } else
+        print_char('\n');
+}
---
+++ 0x08-recursion/main.h
@@ -0,0 +1,6 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+#include <unistd.h>
+
+void _puts_recursion(char *s);
+#endif /**_MAIN_H_*/

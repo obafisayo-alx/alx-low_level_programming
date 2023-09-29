7e3f00b830944f0736059cb9ece1532fb002f916
obafisayo-alx <sekaniisjoy@gmail.com>
2023-09-29 01:31:20 +0100

    ok

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,9 +1,18 @@
 #include "main.h"
-
+/**
+ * print_char - This is the function that is used to print each character.
+ * @c: This is the characer to be printed.
+ */
 void print_char(char c)
 {
     write(1, &c, 1);
 }
+
+/**
+ * _puts_recursion - This is the recursive function
+ * @s: This is the string passed into this functon.
+ * 
+*/
 void _puts_recursion(char *s)
 {
     if (*s != '\0')

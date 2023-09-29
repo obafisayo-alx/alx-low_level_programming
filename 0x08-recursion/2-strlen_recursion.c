ae1a2600edd4ab2175ce6659887bb2c43f41347f
obafisayo-alx <sekaniisjoy@gmail.com>
2023-09-28 18:54:51 -0700

    betty style

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -3,15 +3,16 @@
 /**
  * _strlen_recursion - This is the recursive function.
  * @s: This is the string to be passed.
- * Return - The return value is the length of the string.
+ * Return: The return value is the length of the string.
 */
 int _strlen_recursion(char *s)
 {
-    int len = 0;
-    if (*s != '\0')
-    {
-        len ++;
-        len += _strlen_recursion(s + 1);
-    }
-    return (len);
+	int len = 0;
+
+	if (*s != '\0')
+	{
+		len++;
+		len += _strlen_recursion(s + 1);
+	}
+	return (len);
 }

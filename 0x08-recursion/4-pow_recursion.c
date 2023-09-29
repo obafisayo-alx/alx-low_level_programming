c4acde45b8a7f12c3612b5d80ebfb2c4a49473c7
obafisayo-alx <sekaniisjoy@gmail.com>
2023-09-29 03:42:09 +0100

    added the second file

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -4,6 +4,7 @@
  * _pow_recursion - This is the recursion function.
  * @x: This is the value.
  * @y: This is the power.
+ * Return: This is the return value of the function.
 */
 int _pow_recursion(int x, int y)
 {

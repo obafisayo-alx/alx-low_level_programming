f71e4abd54e39c1e70a68dd91860874ba29d0677
obafisayo-alx <sekaniisjoy@gmail.com>
2023-10-04 15:11:50 +0100

    added the file for the first task

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -4,6 +4,7 @@
  * create_array - This function is used to create an array
  * @size: This is the size of the array
  * @c: This is the character to initialize it with
+ * Return: This is the return value and it is an array.
 */
 char *create_array(unsigned int size, char c)
 {

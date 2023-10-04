05d97928e85a1eb8508ab86915bc4d259946c43b
obafisayo-alx <sekaniisjoy@gmail.com>
2023-10-04 15:10:21 +0100

    added the file for the first task

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,18 +11,11 @@ char *create_array(unsigned int size, char c)
     unsigned int i;
 
     if (size == 0)
-    {
         return (NULL);
-    }
     ar = malloc(sizeof(char) * size);
     if (ar == NULL)
-    {
         return (NULL);
-    }
     for (i = 0; i < size; i++)
-    {
         ar[i] = c;
-    }
-    
     return (ar);
 }

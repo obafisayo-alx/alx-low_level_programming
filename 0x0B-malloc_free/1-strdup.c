4d1d37a4f6d6918d71b3136e75ef1d5c634d40c8
obafisayo-alx <sekaniisjoy@gmail.com>
2023-10-04 19:42:33 -0700

    i

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,27 +1,27 @@
 #include "main.h"
 
 /**
- * _strdup - This function works like the strdup function that is use to duplicate strings.
+ * _strdup - This function works like the strings.
  * @str: This is the string to be duplicated.
- * Return: A pointer to a new string which is a duplicate of the string str. 
+ * Return: A pointer to a new string which is a duplicate of the string str.
 */
 char *_strdup(char *str)
 {
-char *dupstr;
-unsigned int i, j = 0;
+	char *dupstr;
+	unsigned int i, j = 0;
 
-if (str == NULL)
-return (NULL);
-for (i = 0; str[i] != '\0'; i++)
-;
-dupstr = malloc(sizeof(char) * (i + 1));
-if (dupstr == NULL)
-return (NULL);
-while (j <= i)
-{
-dupstr[j] = str[j];
-j++;
-}
-dupstr[i] = '\0';
-return (dupstr);
+	if (str == NULL)
+		return (NULL);
+	for (i = 0; str[i] != '\0'; i++)
+		;
+	dupstr = malloc(sizeof(char) * (i + 1));
+	if (dupstr == NULL)
+		return (NULL);
+	while (j <= i)
+	{
+		dupstr[j] = str[j];
+		j++;
+	}
+	dupstr[i] = '\0';
+	return (dupstr);
 }

0a3e2ee0ca6800b803b71bdd7d56c472ca5a7188
obafisayo-alx <sekaniisjoy@gmail.com>
2023-10-05 02:55:34 +0100

    added the third file

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+int **alloc_grid(int width, int height)
+{
+    int **grid;
+    size_t i, j;
+
+    i = j = 0;
+    if (height <= 0 || width <= 0)
+        return (NULL);
+    grid = (int **)malloc(sizeof(int *) * height);
+    if (grid == NULL)
+        return (NULL);
+    for (i = 0; i < height; i++)
+    {
+        grid[i] = (int *)malloc(sizeof(int) * width);
+        if (grid[i] == NULL)
+        {
+            for (j = 0; j < i; j++)
+                free(grid);
+            free(grid);
+            return (NULL);
+        }
+        for (j = 0; j < width; j++)
+            grid[i][j] = 0;
+    }
+    return (grid);
+}

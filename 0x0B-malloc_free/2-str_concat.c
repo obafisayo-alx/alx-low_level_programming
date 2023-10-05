c4b19adff53d04ed424c4cb730a0c1effc272377
obafisayo-alx <sekaniisjoy@gmail.com>
2023-10-05 01:49:53 +0100

    added string concat file

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,5 +1,11 @@
 #include "main.h"
 
+/**
+ * str_concat - This funcion is used to concatenate two strings together
+ * @s1: This is the first string
+ * @s2: This is the second string
+ * Return: This is a string containing both string 1 and 2 together.
+*/
 char *str_concat(char *s1, char *s2)
 {
     char *catstr;
@@ -14,7 +20,7 @@ char *str_concat(char *s1, char *s2)
         ;
     for (; s2[j] != '\0'; j++)
         ;
-    len = i + j;
+    len = i + j + 1;
     catstr = (char *)malloc(sizeof(char) * len);
     if (catstr == NULL)
         return (NULL);

0036b5c0cd38b90a4b1aac5b3a385c198e667e66
obafisayo-alx <sekaniisjoy@gmail.com>
2023-10-09 22:44:06 +0100

    ls

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,8 +1,13 @@
 #include "main.h"
 
+/**
+* malloc_checked - This is used to create a malloc
+* @b: This is the nmemb of memory to be created
+* Return: This returns a pointer
+*/
 void* malloc_checked(unsigned int b)
 {
-	char* ptr;
+	char *ptr;
 
 	ptr = malloc(sizeof(char) * b);
 	if (ptr == NULL)

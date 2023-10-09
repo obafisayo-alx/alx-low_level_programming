d556f463ffaacd98ddb2f607b0d87f00dd6e79f7
obafisayo-alx <sekaniisjoy@gmail.com>
2023-10-09 13:25:34 +0100

    ok

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,6 +1,13 @@
 #include "main.h"
 
-void *malloc_checked(unsigned int b)
+void* malloc_checked(unsigned int b)
 {
-    char
+	char* ptr;
+
+	ptr = malloc(sizeof(char) * b);
+	if (ptr == NULL)
+	{
+		exit(98);
+	}
+	return (ptr);
 }

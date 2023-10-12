4fe0a525e20e2480bab73768943de3a319628fc2
obafisayo-alx <sekaniisjoy@gmail.com>
2023-10-13 00:52:54 +0100

    file 1 debug

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -17,6 +17,10 @@ void array_iterator(int *array, size_t size, void (*action)(int))
     {
         return;
     }
+    if (array == NULL)
+    {
+        return;
+    }
     for (i = 0; i < size; i++)
     {
         action(array[i]);

43730c414b395d1827963c4f89b1f540fe73b389
obafisayo-alx <sekaniisjoy@gmail.com>
2023-10-12 02:35:45 +0100

    new dog file is debugged

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -61,12 +61,3 @@ dog_t *new_dog(char *name, float age, char *owner)
     p->age = age;
     return (p);
 }
-
-int main(void)
-{
-    dog_t *my_dog;
-
-    my_dog = new_dog("Poppy", 3.5, "Bob");
-    printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog->name, my_dog->age);
-    return (0);
-}

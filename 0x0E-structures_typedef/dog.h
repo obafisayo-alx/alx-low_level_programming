c9ef228c4a96885226774773bb4984934373593a
obafisayo-alx <sekaniisjoy@gmail.com>
2023-10-12 01:34:25 +0100

    dog_t typedef'ed

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -12,17 +12,17 @@
  * Description: This struct just holds the little details about my dog
 */
 
-/**
- * dog_t - Typedef for struct dog_t
-*/
-typedef struct dog dog_t;
-
 struct dog
 {
     char *name;
     char *owner;
     float age;
 };
+
+/**
+ * dog_t - Typedef for struct dog_t
+*/
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);

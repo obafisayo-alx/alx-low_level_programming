727295c94c2122765b9bc351c28afca8b68dc711
obafisayo-alx <sekaniisjoy@gmail.com>
2023-10-13 17:34:11 +0100

    un rev

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -12,5 +12,30 @@
  */
 int main(int argc, char *argv[])
 {
-	
+	int retval, num1, num2;
+	char *operator;
+	int (*retptr)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	operator = argv[2];
+	if ((operator[0] == '/' || operator[0] == '%') && num2 == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	retptr = get_op_func(operator);
+	if (retptr == NULL)
+	{
+		puts("Error");
+		exit(99);
+	}
+	retval = retptr(num1, num2);
+	printf("%d\n", retval);
+	exit(EXIT_SUCCESS);
 }

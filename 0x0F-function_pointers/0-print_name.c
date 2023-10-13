91cf774845ff7e0e69a69252bca4fa14f6aaae55
obafisayo-alx <sekaniisjoy@gmail.com>
2023-10-13 01:06:40 +0100

    betty again

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -8,10 +8,10 @@
 */
 void print_name(char *name, void (*f)(char *))
 {
-    if (*f == NULL)
-    {
-        return;
-    }
-    f(name);
-    return;
+	if (*f == NULL)
+	{
+	return;
+	}
+	f(name);
+	return;
 }

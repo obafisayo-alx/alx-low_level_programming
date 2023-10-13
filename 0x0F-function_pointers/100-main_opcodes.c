b70601590c99ed1a410871dc2db2409150b10e0e
obafisayo-alx <sekaniisjoy@gmail.com>
2023-10-13 19:10:17 +0100

    done

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -10,5 +10,27 @@
  */
 int main(int argc, char *argv[])
 {
-	
+	short nbytes, i;
+
+	if (argc != 2)
+	{
+		puts("Error");
+		exit(1);
+	}
+	nbytes = atoi(argv[1]);
+	if (nbytes < 0)
+	{
+		puts("Error");
+		exit(2);
+	}
+	if (nbytes == 0)
+	{
+		putchar('\n');
+		exit(EXIT_SUCCESS);
+	}
+	printf("%02x", *((unsigned char *) (main)));
+	for (i = 1; i < nbytes; ++i)
+		printf(" %02x", *((unsigned char *) (main + i)));
+	putchar('\n');
+	exit(EXIT_SUCCESS);
 }

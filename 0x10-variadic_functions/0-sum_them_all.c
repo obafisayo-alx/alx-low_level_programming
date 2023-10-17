f8fcbd5ec086570ebb41c6d2472cc91dc7b0c70a
obafisayo-alx <sekaniisjoy@gmail.com>
2023-10-17 18:54:16 +0100

    It's nothing much

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -10,12 +10,12 @@ int sum_them_all(const unsigned int n, ...)
 {
 	va_list ap;
 	int sum = 0;
-	unsigned int c = n;
+	unsigned int i = n;
 
 	if (n == 0)
 		return (0);
 	va_start(ap, n);
-	while (c--)
+	while (i--)
 		sum += va_arg(ap, int);
 	va_end(ap);
 	return (sum);

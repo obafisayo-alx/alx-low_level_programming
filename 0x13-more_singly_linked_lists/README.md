2de4b3e6b73d2d88f7f0303687b10074b6647666
obafisayo-alx <sekaniisjoy@gmail.com>
2023-10-23 10:52:02 +0100

    started the new tasks

---
+++ 0x13-more_singly_linked_lists/README.md
@@ -0,0 +1,9 @@
+# 0x13-more_singly_linked_lists
+## This is a readme file for this folder
+### Task 0
+- Write a function that prints all the elements of a listint_t list.
+
+Prototype: size_t print_listint(const listint_t *h);
+Return: the number of nodes
+Format: see example
+You are allowed to use printf
---
+++ 0x13-more_singly_linked_lists/lists.h
@@ -0,0 +1,22 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <stddef.h>
+#include <stdio.h>
+#include <string.h>
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * 
+ */
+typedef struct listint_s
+{
+    int n;
+    struct listint_s *next;
+} listint_t;
+
+size_t print_listint(const listint_t *h);
+#endif

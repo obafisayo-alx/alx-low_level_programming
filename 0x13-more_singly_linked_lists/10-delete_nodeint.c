84e1511d4ba3957d14fb78d26889a31964df278f
obafisayo-alx <sekaniisjoy@gmail.com>
2023-10-24 16:55:04 +0100

    added task 10

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	unsigned int i;
+	listint_t *temp_node, *current = *head;
+
+	if (*head == NULL)
+		return (-1);
+	if (index == 0)
+	{
+		*head = current->next;
+		free(current);
+		return (1);
+	}
+	for (i = 0; i < index - 1; i++)
+	{
+		if (current == NULL)
+			return (-1);
+		if (current->next == NULL)
+			return (-1);
+		current = current->next;
+	}
+	temp_node = current->next;
+	if (temp_node == NULL)
+		return (-1);
+	current->next = temp_node->next;
+	free(temp_node);
+	return (1);
+}

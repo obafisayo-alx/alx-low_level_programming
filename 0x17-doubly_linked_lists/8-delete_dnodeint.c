13e8b526ce884aa3a358687848432d1b20c6c0ac
obafisayo-alx <sekaniisjoy@gmail.com>
2023-12-08 00:48:44 +0100

    100

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -13,7 +13,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 
 	if (*head == NULL)
 		return (-1);
-	current = *head; 
+	current = *head;
 	if (index == 0)
 	{
 		if (current->next != NULL)

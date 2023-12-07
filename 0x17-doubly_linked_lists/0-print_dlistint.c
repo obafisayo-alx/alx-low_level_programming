cf0d3c5a3060329f61fd655b71689d318eaee8bc
obafisayo-alx <sekaniisjoy@gmail.com>
2023-12-07 21:07:52 +0100

    added task 0

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+size_t print_dlistint(const dlistint_t *h)
+{
+     size_t i;
+
+     for (i = 0; h != NULL; i++)
+     {
+          printf("%d\n", h->n);
+          h = h->next;
+     }
+     return (i);
+}
---
+++ 0x17-doubly_linked_lists/README.md
@@ -0,0 +1,2 @@
+# 0x17-doubly_linked_lists
+## This is a Readme for this repository

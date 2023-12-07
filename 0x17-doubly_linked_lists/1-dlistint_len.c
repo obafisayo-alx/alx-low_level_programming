863002c5580e92fbc2948d896b8edb37778c3756
obafisayo-alx <sekaniisjoy@gmail.com>
2023-12-07 21:23:46 +0100

    added betty for task 1

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,12 +1,17 @@
 #include "lists.h"
 
+/**
+ * dlistint_len - this calculates the length of the list
+ * @h: the pointer to the head of the node
+ * Return: the number of the nodes found
+*/
 size_t dlistint_len(const dlistint_t *h)
 {
-     size_t i;
+	size_t i;
 
-     for (i = 0; h != NULL; i++)
-     {
-          h = h->next;
-     }
-     return (i);
+	for (i = 0; h != NULL; i++)
+	{
+		h = h->next;
+	}
+	return (i);
 }

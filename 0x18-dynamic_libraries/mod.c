8108071c2bb35e36e1a24382f921c81b690c7df0
obafisayo-alx <sekaniisjoy@gmail.com>
2023-12-11 22:47:24 +0100

    omo

--- 0x18-dynamic_libraries/mod.c
+++ 0x18-dynamic_libraries/mod.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * add - carries out the add operation
+ * mod - carries out the mod operation
  * @a: the first val
  * @b: the second val
  * Return: the result of the operation
 */
-int add(int a, int b)
+int mod(int a, int b)
 {
 	return (a % b);
 }

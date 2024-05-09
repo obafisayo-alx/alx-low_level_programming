8ea3cc7415183cfee432b5e500f05e6a0c0cb624
obafisayo-alx <sekaniisjoy@gmail.com>
2024-05-09 03:18:19 +0100

    betty checks completed for task 0-linear.c

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -6,21 +6,21 @@
  * @array: pointer to the first element of the array to search in
  * @size: the number of elements in array
  * @value: is the value to search for
- * 
+ *
  * Return: value on success and -1 when value is not present in the array
  * or the array is NULL
 */
 int linear_search(int *array, size_t size, int value)
 {
-     size_t i = 0;
+	size_t i = 0;
 
-     if (array == NULL)
-          return (-1);
-     for (i = 0; i < size; i++)
-     {
-          printf("Value checked array[%ld] = [%d] \n", i, array[i]);
-          if (array[i] == value)
-               return (i);
-     }
-     return (-1);
+	if (array == NULL)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%ld] = [%d] \n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+	}
+	return (-1);
 }
---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,10 @@
+#include "search_algos.h"
+
+/**
+ * binary_search - searches for a value in a sorted array of integers
+ * using the binary search algorithm
+*/
+int binary_search(int *array, size_t size, int value)
+{
+
+}

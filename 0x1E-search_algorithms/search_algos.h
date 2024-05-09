16fc84916223547c3b584c5fce62da076f6884fc
obafisayo-alx <sekaniisjoy@gmail.com>
2024-05-09 03:09:11 +0100

    dinitialized a readme file, added header file search_algos.h and added task 0-linear.c

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,10 @@
+#ifndef _SEARCH_ALGOS_H_
+#define _SEARCH_ALGOS_H_
+
+/**include headers*/
+#include <stdio.h>
+
+/**Prototypes*/
+int linear_search(int *array, size_t size, int value);
+
+#endif /** _SEARCH_ALGOS_H_**/
